2410b54a3e7394b1825e24945e4ce9886bcd9920
천영성 <hokoro123@gmail.com>
2024-10-09 03:36:10 +0900

    UserCreateFormDTO 작성 - user_id - password

---
+++ src/main/java/com/example/spring/todolist/dto/UserCreateFormDTO.java
@@ -0,0 +1,11 @@
+package com.example.spring.todolist.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateFormDTO {
+    private String user_id;
+    private String password;
+}
