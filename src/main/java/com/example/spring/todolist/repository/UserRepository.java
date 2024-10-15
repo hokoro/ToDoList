1e7cdf65b22887ad46e9e26da5201e74cc40e313
천영성 <hokoro123@gmail.com>
2024-10-15 16:31:26 +0900

    UserRespository = 유저의 아이디가 존재하는지 여부 판별 UserServiceImpl = 유저의 회원가입 기능 API 구현

--- src/main/java/com/example/spring/todolist/repository/UserRepository.java
+++ src/main/java/com/example/spring/todolist/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.example.spring.todolist.repository;
 
 import com.example.spring.todolist.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+    @Query("SELECT u FROM User u WHERE u.user_id = :user_id")
+    User findByUser_id(@Param("user_id") String user_id);
 
 }
--- src/main/java/com/example/spring/todolist/service/UserServiceImpl.java
+++ src/main/java/com/example/spring/todolist/service/UserServiceImpl.java
@@ -1,13 +1,53 @@
 package com.example.spring.todolist.service;
 
 
+import com.example.spring.todolist.domain.User;
+import com.example.spring.todolist.dto.UserCreateFormDTO;
+import com.example.spring.todolist.dto.UserResponseFormDTO;
+import com.example.spring.todolist.repository.UserRepository;
+import com.example.spring.todolist.service.interfaces.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+
+    @Override
+    public ResponseEntity<UserResponseFormDTO> create(UserCreateFormDTO userCreateFormDTO){
+
+        // blank
+        if(userCreateFormDTO.getUser_id().isBlank() || userCreateFormDTO.getPassword().isBlank()){
+            return new ResponseEntity<>(new UserResponseFormDTO("데이터가 존재하지 않습니다.") , HttpStatus.BAD_REQUEST);
+        }
+
+
+
+        // Success
+        Optional<User>member = Optional.ofNullable(userRepository.findByUser_id(userCreateFormDTO.getUser_id()));
+
+        if(member.isEmpty()){   // 아이디 가입이 가능할 경우
+            User newUser = User.builder().
+                    user_id(userCreateFormDTO.getUser_id()).
+                    password(passwordEncoder.encode(userCreateFormDTO.getPassword())).
+                    build();
+            userRepository.save(newUser);
+            return new ResponseEntity<>(new UserResponseFormDTO("회원가입에 성공하였습니다") , HttpStatus.OK);
+        }else{  // 불가능 할 경우 = 이미 존재하는 아이디
+            return new ResponseEntity<>(new UserResponseFormDTO("이미 존재하는 아이디 입니다") , HttpStatus.CONFLICT);  // 이미 존재할 때는 CONFLICT를 응답으로
+        }
+
+
+    }
 }
